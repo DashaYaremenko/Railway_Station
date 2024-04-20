863b515b7e6f0112a7079b423eaf999af719b6fd
DashaYaremenko <dariayaremenko@gmail.com>
2024-04-20 11:54:22 +0300

    y

--- src/main/java/com/example/railway_station/RecTrain.java
+++ src/main/java/com/example/railway_station/RecTrain.java
@@ -1,6 +1,22 @@
 package com.example.railway_station;
 
+import com.example.railway_station.TableClasses.TrainClass;
+import javafx.scene.control.*;
+
 public class RecTrain {
+    private static final String URL = "jdbc:mysql://localhost:3306/railwaystat";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "1111";
+    public TextField IDTrain, NameTrain, CruiseID,DateAndTime;
+    public SplitMenuButton TypeTrain;
+    public Button AddTrainButton,DeleteTrainButton,UpdateTrainButton,LookTrainButton;
+    private TableView <TrainClass> TrainTable;
+    private TableColumn <TrainClass, Integer> IDTrainCol,CruiseIDCol ;
+    private TableColumn<TrainClass,String> NameTrainCol,  DateAndTimeCol, TypeTrain;
+
+
+
+
 
 
 }
