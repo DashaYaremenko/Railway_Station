d53b06610d03e0ff72c42d5607699a762d2482eb
DashaYaremenko <dariayaremenko@gmail.com>
2024-04-24 14:03:02 +0300

    y

--- src/main/java/com/example/railway_station/TableClasses/CarriageClass.java
+++ src/main/java/com/example/railway_station/TableClasses/CarriageClass.java
@@ -4,10 +4,12 @@ public class CarriageClass {
     private String ID;
     private String TypeCarrig;
     private int TrainID;
-    private CarriageClass(String id, String typeCarrig, int trainID) {
+    private int NumSeats;
+    private CarriageClass(String id, String typeCarrig, int trainID, int numSeats) {
         this.ID = id;
         this.TypeCarrig = typeCarrig;
         this.TrainID = trainID;
+        this.NumSeats = numSeats;
     }
     public String getID() {return ID;}
     public void setID(String id) {this.ID = id;}
@@ -15,4 +17,6 @@ public class CarriageClass {
     public void setTypeCarrig(String typeCarrig) {this.TypeCarrig = typeCarrig;}
     public int getTrainID() {return TrainID;}
     public void setTrainID(int trainID) {this.TrainID = trainID;}
+    public int getNumSeats() {return NumSeats;}
+    public void setNumSeats(int numSeats) {this.NumSeats = numSeats;}
 }
