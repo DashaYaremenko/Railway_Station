238229a0829452be2f75528b0931f99c12a931cf
DashaYaremenko <dariayaremenko@gmail.com>
2024-05-18 11:24:48 +0300

    y

--- src/main/java/com/example/railway_station/BuyTicket.java
+++ src/main/java/com/example/railway_station/BuyTicket.java
@@ -1,123 +1,123 @@
-package com.example.railway_station;
-
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.*;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-public class BuyTicket {
-    private static final String URL = "jdbc:mysql://localhost:3306/railwaystat";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "1111";
-    @FXML
-    private Button BuyTicketBtn;
-    @FXML
-    private TextField LastName, FirstName,IdTrain, IdCarriage;
-    @FXML
-    private MenuButton TypeDoc;
-    @FXML
-    private CheckBox Linens, Drink, Snacks;
-    @FXML
-    private Label Cost;
-    @FXML
-    private DatePicker datePicker;
-
-    @FXML
-    private void handleTypeDocSelection(ActionEvent event) {
-        MenuItem menuItem = (MenuItem) event.getSource();
-        String selectedTypeDoc = menuItem.getText();
-        addTypeDoc(selectedTypeDoc);
-    }
-    private void addTypeDoc(String selectedTypeDoc) {
-        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
-            String sql = "INSERT INTO clients (TypeDoc) VALUES (?)";
-            try (PreparedStatement statement = connection.prepareStatement(sql)) {
-                statement.setString(1, selectedTypeDoc);
-                statement.executeUpdate();
-                System.out.println("Тип документа успішно додано до бази даних: " + selectedTypeDoc);
-            } catch (SQLException e) {
-                throw new RuntimeException("Помилка при вставці типу документа: " + e.getMessage());
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException("Помилка з'єднання з базою даних: " + e.getMessage());
-        }
-    }
-
-    private void addService(String service, double cost) {
-        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
-            String sql = "INSERT INTO tickets (Service, CostTicket) VALUES (?, ?)";
-            try (PreparedStatement statement = connection.prepareStatement(sql)) {
-                statement.setString(1, service);
-                statement.setDouble(2, cost);
-                statement.executeUpdate();
-                System.out.println("Послуга успішно додана до бази даних: " + service);
-            } catch (SQLException e) {
-                throw new RuntimeException("Помилка при вставці послуги: " + e.getMessage());
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException("Помилка з'єднання з базою даних: " + e.getMessage());
-        }
-    }
-
-    @FXML
-    private void handleBuyTicket(ActionEvent event) {
-        double totalPrice = 0.0;
-
-        // Отримуємо прізвище та ім'я клієнта з текстових полів
-        String lastName = LastName.getText();
-        String firstName = FirstName.getText();
-
-        // Перевіряємо, які послуги вибрані та додаємо їх до бази даних
-        if (Linens.isSelected()) {
-            addService("Linens", 10.0);
-            totalPrice += 10.0;
-        }
-        if (Drink.isSelected()) {
-            addService("Drink", 5.0);
-            totalPrice += 5.0;
-        }
-        if (Snacks.isSelected()) {
-            addService("Snacks", 7.0);
-            totalPrice += 7.0;
-        }
-
-        // Оновлюємо відображення загальної ціни на мітці Price
-        Cost.setText(String.valueOf(totalPrice));
-
-        // Додаємо інформацію про клієнта до бази даних
-        addClient(lastName, firstName);
-    }
-
-
-
-    @FXML
-    private void ByeTicketAction(ActionEvent event) {
-        String LastN = LastName.getText();
-        String FirstN = FirstName.getText();
-        String CarId = CarriageID.getText();
-        String TypeT = TypeTrain.getText();
-
-        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
-            String sql = "INSERT INTO trains (ID, NameM, TypeTrain,CruiseID,CarriageID) VALUES (?,?,?,?,?)";
-            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-                preparedStatement.setString(1, Id);
-                preparedStatement.setString(2, NameT);
-                preparedStatement.setString(3, TypeT);
-                preparedStatement.setString(4, CruID);
-                preparedStatement.setString(5, CarId);
-                int rowsAdd = preparedStatement.executeUpdate();
-                if (rowsAdd > 0) {
-                    System.out.println("Запис добавлено успішно");
-                    ShowButtonAction(event);
-                }
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-}
+//package com.example.railway_station;
+//
+//import javafx.event.ActionEvent;
+//import javafx.fxml.FXML;
+//import javafx.scene.control.*;
+//
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.PreparedStatement;
+//import java.sql.SQLException;
+//
+//public class BuyTicket {
+//    private static final String URL = "jdbc:mysql://localhost:3306/railwaystat";
+//    private static final String USERNAME = "root";
+//    private static final String PASSWORD = "1111";
+//    @FXML
+//    private Button BuyTicketBtn;
+//    @FXML
+//    private TextField LastName, FirstName,IdTrain, IdCarriage;
+//    @FXML
+//    private MenuButton TypeDoc;
+//    @FXML
+//    private CheckBox Linens, Drink, Snacks;
+//    @FXML
+//    private Label Cost;
+//    @FXML
+//    private DatePicker datePicker;
+//
+//    @FXML
+//    private void handleTypeDocSelection(ActionEvent event) {
+//        MenuItem menuItem = (MenuItem) event.getSource();
+//        String selectedTypeDoc = menuItem.getText();
+//        addTypeDoc(selectedTypeDoc);
+//    }
+//    private void addTypeDoc(String selectedTypeDoc) {
+//        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+//            String sql = "INSERT INTO clients (TypeDoc) VALUES (?)";
+//            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+//                statement.setString(1, selectedTypeDoc);
+//                statement.executeUpdate();
+//                System.out.println("Тип документа успішно додано до бази даних: " + selectedTypeDoc);
+//            } catch (SQLException e) {
+//                throw new RuntimeException("Помилка при вставці типу документа: " + e.getMessage());
+//            }
+//        } catch (SQLException e) {
+//            throw new RuntimeException("Помилка з'єднання з базою даних: " + e.getMessage());
+//        }
+//    }
+//
+//    private void addService(String service, double cost) {
+//        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+//            String sql = "INSERT INTO tickets (Service, CostTicket) VALUES (?, ?)";
+//            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+//                statement.setString(1, service);
+//                statement.setDouble(2, cost);
+//                statement.executeUpdate();
+//                System.out.println("Послуга успішно додана до бази даних: " + service);
+//            } catch (SQLException e) {
+//                throw new RuntimeException("Помилка при вставці послуги: " + e.getMessage());
+//            }
+//        } catch (SQLException e) {
+//            throw new RuntimeException("Помилка з'єднання з базою даних: " + e.getMessage());
+//        }
+//    }
+//
+//    @FXML
+//    private void handleBuyTicket(ActionEvent event) {
+//        double totalPrice = 0.0;
+//
+//        // Отримуємо прізвище та ім'я клієнта з текстових полів
+//        String lastName = LastName.getText();
+//        String firstName = FirstName.getText();
+//
+//        // Перевіряємо, які послуги вибрані та додаємо їх до бази даних
+//        if (Linens.isSelected()) {
+//            addService("Linens", 10.0);
+//            totalPrice += 10.0;
+//        }
+//        if (Drink.isSelected()) {
+//            addService("Drink", 5.0);
+//            totalPrice += 5.0;
+//        }
+//        if (Snacks.isSelected()) {
+//            addService("Snacks", 7.0);
+//            totalPrice += 7.0;
+//        }
+//
+//        // Оновлюємо відображення загальної ціни на мітці Price
+//        Cost.setText(String.valueOf(totalPrice));
+//
+//        // Додаємо інформацію про клієнта до бази даних
+//        addClient(lastName, firstName);
+//    }
+//
+//
+//
+//    @FXML
+//    private void ByeTicketAction(ActionEvent event) {
+//        String LastN = LastName.getText();
+//        String FirstN = FirstName.getText();
+//        String CarId = CarriageID.getText();
+//        String TypeT = TypeTrain.getText();
+//
+//        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+//            String sql = "INSERT INTO trains (ID, NameM, TypeTrain,CruiseID,CarriageID) VALUES (?,?,?,?,?)";
+//            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+//                preparedStatement.setString(1, Id);
+//                preparedStatement.setString(2, NameT);
+//                preparedStatement.setString(3, TypeT);
+//                preparedStatement.setString(4, CruID);
+//                preparedStatement.setString(5, CarId);
+//                int rowsAdd = preparedStatement.executeUpdate();
+//                if (rowsAdd > 0) {
+//                    System.out.println("Запис добавлено успішно");
+//                    ShowButtonAction(event);
+//                }
+//            }
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//
+//}
--- src/main/java/com/example/railway_station/SearchTrain.java
+++ src/main/java/com/example/railway_station/SearchTrain.java
@@ -12,6 +12,7 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.sql.*;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class SearchTrain {
@@ -41,6 +42,84 @@ public class SearchTrain {
     @FXML
     private TableColumn<CruiseClass,Timestamp> TimeTrainColumn2;
 
+    private void handleSearch() {
+        LocalDate selectedDate = datePicker.getValue();
+        if (selectedDate != null) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Дата вибрана");
+            alert.setHeaderText(null);
+            alert.setContentText("Обрана дата: " + selectedDate.toString());
+            alert.showAndWait();
+        }
+    }
+
+    @FXML
+    private void STButtonAction(ActionEvent event) {
+        handleSearch();
+        LocalDate selectedDate = datePicker.getValue();
+        if (selectedDate == null) {
+            return;
+        }
+        ObservableList<TrainClass> dataList = FXCollections.observableArrayList();
+        ObservableList<StationClass> dataList1 = FXCollections.observableArrayList();
+        ObservableList<CruiseClass> dataList2 = FXCollections.observableArrayList();
+        ObservableList<CarriageClass> dataList3 = FXCollections.observableArrayList();
+        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+            String sql = "SELECT t.ID, t.TypeCru, t.ColCruise, s.NameStation, c.TypeCarrig, c.NumSeats, ts.ArrivTime, ts.DeparTime " +
+                    "FROM trains t " +
+                    "JOIN stations s ON t.StationID = s.ID " +
+                    "JOIN carriages c ON t.ID = c.TrainID " +
+                    "JOIN trainstation ts ON t.ID = ts.TrainId";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(sql);
+                 ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    String id = resultSet.getString("ID");
+                    String nameM1 = resultSet.getString("NameM1");
+                    String nameM2 = resultSet.getString("NameM2");
+                    String typeCru = resultSet.getString("TypeCru");
+                    LocalDateTime timeTrain1 = resultSet.getTimestamp("TimeTrain1").toLocalDateTime();
+                    LocalDateTime timeTrain2 = resultSet.getTimestamp("TimeTrain2").toLocalDateTime();
+                    int colCruise = resultSet.getInt("ColCruise");
+                    TrainClass train = new TrainClass(id);
+                    dataList.add(train);
+                    StationClass stationClass = new StationClass(nameM1, nameM2);
+                    dataList1.add(stationClass);
+                    CruiseClass cruiseClass = new CruiseClass(timeTrain1, timeTrain2);
+                    dataList2.add(cruiseClass);
+                    String typeCarriage = resultSet.getString("TypeCarriage");
+                    int numSeats = resultSet.getInt("NumSeats");
+                    CarriageClass carriage = new CarriageClass(id, typeCarriage, numSeats);
+                    dataList3.add(carriage);
+                }
+
+            }
+            IdColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+            NameM1_Column.setCellValueFactory(new PropertyValueFactory<>("nameM"));
+            NameM2_Column.setCellValueFactory(new PropertyValueFactory<>("nameM"));
+            TimeTrainColumn1.setCellValueFactory(new PropertyValueFactory<>("arrivTime"));
+            TimeTrainColumn2.setCellValueFactory(new PropertyValueFactory<>("deparTime"));
+            ColCruColumn.setCellValueFactory(new PropertyValueFactory<>("colCruise"));
+            TypeCruColumn.setCellValueFactory(new PropertyValueFactory<>("typeCru"));
+            TableShow.setItems(dataList);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+//    SELECT t.ID AS trainID,
+//    t.NameM as NameTrain,
+//    s1.NameStation AS departureStation,
+//    s2.NameStation AS arrivalStation,
+//    ts.DeparTime AS departureTime,
+//    ts.ArrivTime AS arrivalTime,
+//    COUNT(c.ID) AS carriageCount,
+//    GROUP_CONCAT(c.TypeCarrig) AS carriageTypes
+//    FROM trains t
+//    JOIN trainstations ts ON t.ID = ts.TrainId
+//    JOIN stations s1 ON ts.StationID = s1.ID
+//    JOIN stations s2 ON ts.StationID = s2.ID
+//    JOIN carriage c ON t.ID = c.TrainID
+//    GROUP BY t.ID, s1.NameStation, s2.NameStation, ts.DeparTime, ts.ArrivTime
+
 //    @FXML
 //    private void STButtonAction(ActionEvent event) {
 //        ObservableList<TrainClass> dataList = FXCollections.observableArrayList();
@@ -67,10 +146,8 @@ public class SearchTrain {
 //                    StationClass stationClass = new StationClass(resultSet.getInt("StationID"), resultSet.getString("StationName"));
 //                    CruiseClass cruiseClass = new CruiseClass(resultSet.getTimestamp("ArrivTime").toLocalDateTime(), resultSet.getTimestamp("DeparTime").toLocalDateTime());
 //                    CarriageClass carriageClass=new CarriageClass();
-//
 //                    dataList1.add(stationClass);
 //                    dataList2.add(cruiseClass);
-//
 //                }
 //            }
 //            IdColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
@@ -85,56 +162,10 @@ public class SearchTrain {
 //            throw new RuntimeException(e);
 //        }
 //    }
-@FXML
-private void STButtonAction(ActionEvent event) {
-    ObservableList<TrainClass> dataList = FXCollections.observableArrayList();
-    ObservableList<StationClass> dataList1 = FXCollections.observableArrayList();
-    ObservableList<CruiseClass> dataList2 = FXCollections.observableArrayList();
-    ObservableList<CarriageClass> dataList3 = FXCollections.observableArrayList();
-    try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
-        String sql = "SELECT t.ID, t.TypeCru, t.ColCruise, s.NameStation, c.TypeCarrig, c.NumSeats, ts.ArrivTime, ts.DeparTime " +
-                "FROM trains t " +
-                "JOIN stations s ON t.StationID = s.ID " +
-                "JOIN carriages c ON t.ID = c.TrainID " +
-                "JOIN trainstation ts ON t.ID = ts.TrainId";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql);
-             ResultSet resultSet = preparedStatement.executeQuery()) {
-            while (resultSet.next()) {
-                String id = resultSet.getString("ID");
-                String nameM1 = resultSet.getString("NameM1");
-                String nameM2 = resultSet.getString("NameM2");
-                String typeCru = resultSet.getString("TypeCru");
-                LocalDateTime timeTrain1 = resultSet.getTimestamp("TimeTrain1").toLocalDateTime();
-                LocalDateTime timeTrain2 = resultSet.getTimestamp("TimeTrain2").toLocalDateTime();
-                int colCruise = resultSet.getInt("ColCruise");
-                TrainClass train = new TrainClass(id);
-                dataList.add(train);
-                StationClass stationClass = new StationClass(nameM1, nameM2);
-                dataList1.add(stationClass);
-                CruiseClass cruiseClass = new CruiseClass(timeTrain1, timeTrain2);
-                dataList2.add(cruiseClass);
-                String typeCarriage = resultSet.getString("TypeCarriage");
-                int numSeats = resultSet.getInt("NumSeats");
-                CarriageClass carriage = new CarriageClass(id, typeCarriage, numSeats);
-                dataList3.add(carriage);
-            }
-
-        }
-        IdColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
-        NameM1_Column.setCellValueFactory(new PropertyValueFactory<>("nameM"));
-        NameM2_Column.setCellValueFactory(new PropertyValueFactory<>("nameM"));
-        TimeTrainColumn1.setCellValueFactory(new PropertyValueFactory<>("arrivTime"));
-        TimeTrainColumn2.setCellValueFactory(new PropertyValueFactory<>("deparTime"));
-        ColCruColumn.setCellValueFactory(new PropertyValueFactory<>("colCruise"));
-        TypeCruColumn.setCellValueFactory(new PropertyValueFactory<>("typeCru"));
-        TableShow.setItems(dataList);
-    } catch (SQLException e) {
-        throw new RuntimeException(e);
-    }
-}
-
-}
-
 
 
 }
+
+
+
+
