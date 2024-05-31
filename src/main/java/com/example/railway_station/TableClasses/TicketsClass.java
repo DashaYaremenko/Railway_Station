c522b25753622816b212c6225e50fed6584c48d1
DashaYaremenko <dariayaremenko@gmail.com>
2024-05-31 16:04:06 +0300

    y

--- src/main/java/com/example/railway_station/TableClasses/TicketsClass.java
+++ src/main/java/com/example/railway_station/TableClasses/TicketsClass.java
@@ -5,21 +5,27 @@ public class TicketsClass {
     private int NumTrain;
     private int StationID_1;
     private int StationID_2;
-    private int Cruise_ID;
-    private int Client_Id;
+    private int CruiseID1;
+    private int CruiseID2;
+    private int ClientId;
     private int NumCarrig;
     private double CostTicket;
-    private String Service;
-    public TicketsClass(int idTicket,int numTrain, int stationID_1, int stationID_2,int client_Id ,int cruise_ID, int numCarrig, double costTicket, String service ) {
+    private boolean Linens;
+    private boolean Drink;
+    private boolean Snacks;
+    public TicketsClass(int idTicket,int numTrain, int stationID_1, int stationID_2,int clientId ,int cruiseID1,int cruiseID2, int numCarrig, double costTicket, boolean linens, boolean drink, boolean snacks ) {
         this.IDTicket = idTicket;
         this.NumTrain = numTrain;
         this.StationID_1 = stationID_1;
         this.StationID_2 = stationID_2;
-        this.Cruise_ID = cruise_ID;
+        this.CruiseID1 = cruiseID1;
+        this.CruiseID2 = cruiseID2;
         this.NumCarrig = numCarrig;
         this.CostTicket = costTicket;
-        this.Service = service;
-        this.Client_Id=client_Id;
+        this.ClientId=clientId;
+        this.Linens=linens;
+        this.Drink=drink;
+        this.Snacks=snacks;
     }
     public int getIDTicket() {return IDTicket;}
     public void setIDTicket(int idTicket) {this.IDTicket = idTicket;}
@@ -29,14 +35,21 @@ public class TicketsClass {
     public void setStationID_1(int stationID_1) {this.StationID_1 = stationID_1;}
     public int getStationID_2() {return StationID_2;}
     public void setStationID_2(int stationID_2) {this.StationID_2 = stationID_2;}
-    public int getCruise_ID() {return Cruise_ID;}
-    public void setCruise_ID(int cruise_ID) {Cruise_ID = cruise_ID;}
-    public int getClient_Id() {return Client_Id;}
-    public void setClient_Id(int client_Id) {Client_Id = client_Id;}
+    public int getCruiseID1() {return CruiseID1;}
+    public void setCruiseID0(int cruiseID1) {CruiseID1 = cruiseID1;}
+    public int getCruiseID2() {return CruiseID2;}
+    public void setCruiseID2(int cruiseID2) {CruiseID2 = cruiseID2;}
+    public int getClientId() {return ClientId;}
+    public void setClient_Id(int clientId) {ClientId = clientId;}
     public int getNumCarrig() {return NumCarrig;}
     public void setNumCarrig(int numCarrig) {NumCarrig = numCarrig;}
     public double getCostTicket() {return CostTicket;}
     public void setCostTicket(double costTicket) {CostTicket = costTicket;}
-    public String getService() {return Service;}
-    public void setService(String service) {this.Service = service;}
+    public boolean getLinens() {return Linens;}
+    public void setLinens(boolean linens) {Linens = linens;}
+    public boolean getDrink() {return Drink;}
+    public void setDrink(boolean drink) {Drink = drink;}
+    public boolean getSnacks() {return Snacks;}
+    public void setSnacks(boolean snacks) {Snacks = snacks;}
+
 }
