9ea37bd6fe98026ebd40eaec500c9bd77e9bc78e
DashaYaremenko <dariayaremenko@gmail.com>
2024-06-01 22:27:25 +0300

    y

--- src/main/java/com/example/railway_station/RecCruise.java
+++ src/main/java/com/example/railway_station/RecCruise.java
@@ -1,5 +1,20 @@
 package com.example.railway_station;
 
-public class RecCruise {
+import com.example.railway_station.TableClasses.CruiseClass;
+import javafx.scene.control.*;
 
+import java.sql.*;
+
+public class RecCruise {
+    private static final String URL = "jdbc:mysql://localhost:3306/railwaystat";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "1111";
+    public TextField Time;
+    public Button LookTime, FunDate,FunTime;
+    public DatePicker Date;
+    public TableView<CruiseClass> cruiseTableView;
+    public TableColumn<CruiseClass, Time> TimeCol1;
+    public TableColumn<CruiseClass, Date> DateCol1;
+    public TableColumn<CruiseClass, Time> TimeCol2;
+    public TableColumn<CruiseClass, Date> DateCol2;
 }
