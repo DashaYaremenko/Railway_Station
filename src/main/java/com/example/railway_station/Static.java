e7c1ae947cf10a0b0ab1aa8c3eb5ef3a68539a92
DashaYaremenko <dariayaremenko@gmail.com>
2024-06-03 23:13:53 +0300

    y

--- src/main/java/com/example/railway_station/Static.java
+++ src/main/java/com/example/railway_station/Static.java
@@ -1,7 +1,9 @@
 package com.example.railway_station;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.chart.PieChart;
+import javafx.scene.control.Button;
 
 import java.sql.*;
 
@@ -11,8 +13,10 @@ public class Static {
     private static final String PASSWORD = "1111";
 @FXML
 private PieChart staticTicketTrains;
+@FXML
+private Button staticTicketTrainsButton;
 
-public void staticTicTrain() {
+public void staticTicTrain(ActionEvent event) {
         try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
             Statement statement = connection.createStatement();
             String sql = "SELECT t.ID AS train_id, " +
@@ -24,29 +28,22 @@ public void staticTicTrain() {
             ResultSet resultSet = statement.executeQuery(sql);
 
             while (resultSet.next()) {
-                String trainName = resultSet.getString("train_name");
+                String trainName = resultSet.getString("train_id");
                 int ticketCount = resultSet.getInt("total_tickets_sold");
 
                 // Додавання даних до PieChart
-                PieChart.Data slice = new PieChart.Data(trainName, ticketCount);
+                PieChart.Data slice = new PieChart.Data(trainName + " (" + ticketCount + ")", ticketCount);
                 staticTicketTrains.getData().add(slice);
+                slice.getNode().lookup(".chart-pie-label").setStyle("-fx-font-size: 14px;");
+
             }
 
             resultSet.close();
             statement.close();
-            connection.close();
-
         } catch (SQLException ex) {
             throw new RuntimeException(ex);
         }
     }
 
-//запит на загальну кількість білетів
-// SELECT t.ID AS train_id,
-//t.NameM AS train_name,
-//COUNT(tc.TicketID) AS total_tickets_sold
-//FROM trains t
-//JOIN tickets tc ON t.ID = tc.TrainNum
-//GROUP BY t.ID, t.NameM;
 
 }
