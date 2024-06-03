8f3a45dd2b05ceb19fc60a8e0f8b6ff6a14ebe53
DashaYaremenko <dariayaremenko@gmail.com>
2024-06-03 20:46:30 +0300

    y

--- src/main/java/com/example/railway_station/Static.java
+++ src/main/java/com/example/railway_station/Static.java
@@ -1,4 +1,15 @@
 package com.example.railway_station;
 
+import javafx.scene.chart.PieChart;
+
 public class Static {
+private PieChart staticTicketTrains;
+//запит на загальну кількість білетів
+// SELECT t.ID AS train_id,
+//t.NameM AS train_name,
+//COUNT(tc.TicketID) AS total_tickets_sold
+//FROM trains t
+//JOIN tickets tc ON t.ID = tc.TrainNum
+//GROUP BY t.ID, t.NameM;
+
 }
